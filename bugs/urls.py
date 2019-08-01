512ea75a1c6a1b8970aa15a6e86064980eb9a17e
toderas <toaderchirila21@gmail.com>
2019-08-01 21:58:12 +0000

    added url to display single item

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import get_bugs, add_new_bug
+from .views import get_bugs, add_new_bug, get_current_bug
 
 
 urlpatterns = [
     url(r'^current-bugs/$', get_bugs, name="show_bugs"),
     url(r'^add-bug/', add_new_bug, name="add_new_bug"),
+    url(r'^get-bug/(?P<id>\d+)', get_current_bug, name="get-bug"),
 ]
