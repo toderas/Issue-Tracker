39fded783d2ca1e71d31160f3c8a40200245bf58
toderas <toaderchirila21@gmail.com>
2019-08-02 21:53:51 +0000

    retrieves item comments from db and displays single item and form to add comments

--- bugs/views.py
+++ bugs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import bug_item
-from .forms import AddBugForm
+from .models import bug_item, BugComment
+from .forms import AddBugForm, AddCommentForm
 
 # Create your views here.
 def get_bugs(request):
@@ -23,11 +23,28 @@ def add_new_bug(request):
     else:
         item_form = AddBugForm()
     return render(request, 'new-bug.html', {"item_form": item_form})
+#
 
-
+#
 def get_current_bug(request, id):
     bug = get_object_or_404(bug_item, id=id)
     print(bug)
-    return render(request, 'bug-details.html', {'bug': bug})
+    if request.method == 'POST':
+        comment_form = AddCommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = bug
+            comment.author = request.user
+            comment.save()
+            print(comment)
+           # return render(request, 'bug-details.html', {'bug': bug, 'comment': comment, 'comment_form': comment_form})
 
-
+    else:
+        comment_form = AddCommentForm()
+        print(comment_form)
+    comment = BugComment.objects.filter(post_id=bug.id)
+    print(comment)
+    comment_form = AddCommentForm()
+    
+    return render(request, 'bug-details.html', {'bug': bug, 'comment': comment, 'comment_form': comment_form})
+    
