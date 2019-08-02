85de93831d9f1a688c6c2fedf71207923c5cb650
toderas <toaderchirila21@gmail.com>
2019-08-02 21:40:21 +0000

    register comments

--- bugs/admin.py
+++ bugs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import bug_item
+from .models import bug_item, BugComment
 # Register your models here.
 
 admin.site.register(bug_item)
+admin.site.register(BugComment)
