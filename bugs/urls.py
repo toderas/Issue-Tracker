b74fdaa5580dc27f1be8e836088eb2eac19bc259
toderas <toaderchirila21@gmail.com>
2019-08-04 01:58:14 +0000

    added url for deleting comments

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
-from .views import get_bugs, add_new_bug, get_current_bug
+from .views import get_bugs, add_new_bug, get_current_bug, remove_comment
 
 
 urlpatterns = [
     url(r'^current-bugs/$', get_bugs, name="show_bugs"),
     url(r'^add-bug/', add_new_bug, name="add_new_bug"),
     url(r'^get-bug/(?P<id>\d+)', get_current_bug, name="get-bug"),
+    url(r'^remove-comment/(\d+)/$', remove_comment, name="delete-comment"),
 ]
