af6d44a2c7c0701f377ecc8774c6fafccc50a512
toderas <toaderchirila21@gmail.com>
2019-08-07 04:04:14 +0000

    added remove bug url

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from .views import get_bugs, add_new_bug, get_current_bug, remove_comment, add_upvotes
+from .views import get_bugs, add_new_bug, get_current_bug, remove_comment, add_upvotes, delete_bug
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^get-bug/(?P<id>\d+)', get_current_bug, name="get-bug"),
     url(r'^remove-comment/(\d+)/$', remove_comment, name="delete-comment"),
     url(r'^add-upvote/(?P<id>\d+)', add_upvotes, name="add-upvote"),
+    url(r'^delete-bug/(\d+)/$', delete_bug, name="delete-bug"),
 ]
