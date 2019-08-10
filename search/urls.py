7db9985b3d9b915cdce3ca104c984bf8e31336f0
toderas <toaderchirila21@gmail.com>
2019-08-10 14:03:17 +0000

    added search urls for searching by status and searching by username

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
-from .views import search_bugs
+from .views import search_bugs, search_user_bugs, search_in_progress, search_author_bugs, search_pending_review, search_resolved
 
 urlpatterns = [
 #    url(r'^$', do_search, name='search'),
     url(r'^search-bugs/$', search_bugs, name='search-bugs'),
+    url(r'^search-user-bugs/$', search_user_bugs, name='search-user-bugs'),
+    url(r'^search-pending-review/$', search_pending_review, name='Pending-review'),
+    url(r'^search-in-progress/$', search_in_progress, name='in-progress-bugs'),
+    url(r'^search-resolved/$', search_resolved, name='Resolved'),
+    url(r'^search-author-bugs/$', search_author_bugs, name='author-bugs'),
     ]
