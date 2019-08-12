8f4723df7a5d0447af4a5a51fb76aae8caeaf612
toderas <toaderchirila21@gmail.com>
2019-08-12 14:48:07 +0000

    removed unnecessary code

--- bugs/views.py
+++ bugs/views.py
@@ -31,8 +31,6 @@ def add_new_bug(request):
         if item_form.is_valid():
             bug = item_form.save(commit=False)
             bug.author = request.user
-            bug.author_image = request.user.profile.image
-           # bug.upvoted_by = request.user
             bug.save()
             return redirect(get_bugs)
     else:
@@ -53,7 +51,6 @@ def get_current_bug(request, id):
             comment = comment_form.save(commit=False)
             comment.post = bug
             comment.author = request.user
-            comment.author_image = request.user.profile.image
             comment.save()
             return redirect(request.META['HTTP_REFERER'])
     else:
