bd505291b38c7fb3744dacdcbf78adac0aba7c45
toderas <toaderchirila21@gmail.com>
2019-08-14 15:26:45 +0000

    fixed error caused by the dots in bucket name (static files wouldn't load)

--- master/settings.py
+++ master/settings.py
@@ -143,7 +143,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = 'issue-tracker-django.a.a'
+AWS_STORAGE_BUCKET_NAME = 'issue-tracker-django-a-a'
 AWS_S3_REGION_NAME = 'eu-west-2'
 AWS_DEFAULT_ACL = None
 
