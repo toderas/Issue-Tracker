5b08188565de8e8da8ea8a86d760003b889b129a
toderas <toaderchirila21@gmail.com>
2019-08-16 12:45:01 +0000

    added feature model

--- features/models.py
+++ features/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Feature(models.Model):
+    name = models.CharField(max_length=254, default="")
+    description = models.TextField()
+    date_reported = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
