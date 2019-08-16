f598271cc006167c1dfa17bf76887f8f0b57db2e
toderas <toaderchirila21@gmail.com>
2019-08-16 12:46:19 +0000

    added views for viewing registered features and requesting new feature

--- features/views.py
+++ features/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import Feature
+from .forms import AddFeatureForm
 
 # Create your views here.
+def all_features(request):
+    features = Feature.objects.all()
+    return render(request, "features.html", {"features": features})
+    
+
+@login_required()
+def add_new_feature(request):
+    """
+    Renders AddFeatureForm saves its contents and returns to main page 
+    """
+    if request.method == 'POST':
+        feature_form = AddFeatureForm(request.POST)
+        if feature_form.is_valid():
+            feature = feature_form.save(commit=False)
+            feature.author = request.user
+            feature.save()
+            return redirect(all_features)
+    else:
+        feature_form = AddFeatureForm()
+    return render(request, 'request-feature.html', {"feature_form": feature_form})
