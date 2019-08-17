df25fb403c5e0fec3fb4f99a7b9038a2b0af4105
toderas <toaderchirila21@gmail.com>
2019-08-17 10:05:01 +0000

    modified settings to reflect current requirements

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from products.models import Product
+from features.models import Feature
 
 
 def cart_contents(request):
@@ -11,12 +11,12 @@ def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
+    feature_count = 0
     
     for id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=id)
-        total += quantity * product.price
-        product_count += quantity
-        cart_items.append({'id': id, 'quantity': quantity, 'product': product})
+        feature = get_object_or_404(Feature, pk=id)
+        total += quantity * feature.price
+        feature_count += quantity
+        cart_items.append({'id': id, 'quantity': quantity, 'feature': feature})
     
-    return {'cart_items': cart_items, 'total': total, 'product_count': product_count}
+    return {'cart_items': cart_items, 'total': total, 'feature_count': feature_count}
