36a60f275d5d6d9265cae815a242738b2aa0dca6
toderas <toaderchirila21@gmail.com>
2019-08-17 10:06:00 +0000

    added urls for cart and checkout

--- master/urls.py
+++ master/urls.py
@@ -21,6 +21,8 @@ from accounts.views import index
 from bugs.views import get_bugs, add_new_bug
 from search import urls as urls_search
 from features import urls as urls_features
+from cart import urls as urls_cart
+from checkout import urls as urls_checkout
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -30,6 +32,8 @@ urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^bugs', include(urls_bugs)),
     url(r'^features', include(urls_features)),
+    url(r'^cart', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
