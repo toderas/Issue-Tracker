e764b31193467be4262cd9b3474fa1a13484c509
toderas <toaderchirila21@gmail.com>
2019-08-21 12:11:12 +0000

    retrieves single feature and displays it's options

--- features/views.py
+++ features/views.py
@@ -1,7 +1,11 @@
+from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
 from .models import Feature
-from .forms import AddFeatureForm
+from .forms import AddFeatureForm, ContributeFeatureForm
+
 
 # Create your views here.
 def all_features(request):
@@ -24,3 +28,30 @@ def add_new_feature(request):
     else:
         feature_form = AddFeatureForm()
     return render(request, 'request-feature.html', {"feature_form": feature_form})
+    
+@login_required
+def get_current_feature(request, id):
+    """
+    Displays single item  with option to contribute with desired amount
+    """
+    feature = get_object_or_404(Feature, id=id)
+    if feature.target_value == feature.value_collected:
+        progress = 100.0
+    try:
+        progress = (abs(int(feature.value_collected)) / int(feature.target_value)) * 100.0
+        remaining = (abs(int(feature.target_value)) - int(feature.value_collected))
+    except ZeroDivisionError:
+        progress = 0
+    if request.method == 'POST':
+        contribute_form = ContributeFeatureForm(request.POST)
+        if contribute_form.is_valid():
+            amount = contribute_form.save(commit=False)
+            amount.post = feature
+            amount.author = request.user
+            amount.save()
+            return redirect(request.META['HTTP_REFERER'])
+    else:
+        contribute_form = ContributeFeatureForm()
+    
+    return render(request, 'feature-details.html', {'feature': feature, 'contribute_form': contribute_form, 'progress': progress, 'remaining': remaining})
+    
