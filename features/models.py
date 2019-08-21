e00067d18bb5844610cafbf44e44023590d954aa
toderas <toaderchirila21@gmail.com>
2019-08-21 12:12:50 +0000

    added amount fields used for statistics

--- features/models.py
+++ features/models.py
@@ -8,7 +8,9 @@ class Feature(models.Model):
     description = models.TextField()
     date_reported = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    value_collected = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    target_value = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     
     def __str__(self):
         return self.name
