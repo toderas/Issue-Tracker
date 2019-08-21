a71a057ae86cb28b95a38b683b1fda9058bba306
toderas <toaderchirila21@gmail.com>
2019-08-21 12:03:02 +0000

    further styling

--- features/urls.py
+++ features/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_features, add_new_feature
+from .views import all_features, add_new_feature, get_current_feature
 
 urlpatterns = [
     url(r'^$', all_features, name='features'),
     url(r'^add-feature/', add_new_feature, name="add_new_feature"),
+    url(r'^get-feature/(?P<id>\d+)', get_current_feature, name="get-feature"),
 ]
