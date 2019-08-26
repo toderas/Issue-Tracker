faebb442572aa222331724acf316047aedd879b6
toderas <toaderchirila21@gmail.com>
2019-08-26 14:13:00 +0000

    added models for views and feature contributors

--- features/models.py
+++ features/models.py
@@ -2,15 +2,34 @@ from django.db import models
 from datetime import date
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Feature(models.Model):
     name = models.CharField(max_length=254, default="")
     description = models.TextField()
     date_reported = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    amount = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     value_collected = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     target_value = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     
     def __str__(self):
         return self.name
+
+class FeatureContributors(models.Model):
+    user = models.ForeignKey(User, null=True)
+    amount = models.CharField(max_length=254, default="")
+    date_contributed = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Feature, null=True)
+    
+
+class FeatureViews(models.Model):
+    user = models.ForeignKey(User, null=True)
+    post = models.ForeignKey(Feature, null=True)
+    
+    def __str__(self):
+        return self.name
+        
+        
+
+        
