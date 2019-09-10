eb0c56d11201cad284cc6cfa935ae22a2a8a67ff
toderas <toaderchirila21@gmail.com>
2019-09-10 05:13:11 +0000

    added further test units for search querries

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from django.shortcuts import render, redirect, reverse
+from bugs.models import bug_item
+from django.db.models import Q
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class Test_Bugs_Query(TestCase):
+    
+    def setUp(self):
+        
+        name = 'edited bug'
+        bug_item.objects.create(name=name)
+    
+    
+    def test_get_bug_search_results(self):
+        bug = bug_item.objects.get(name__icontains="edited bug")
+        self.assertTrue(bug.name != '')
+        self.assertTrue(bug.name == 'edited bug')
+        self.assertTrue(bug.description == '')
