65f2b4b0045b827474b72b6325c9ae2111a89e2f
toderas <toaderchirila21@gmail.com>
2019-09-10 05:12:26 +0000

    added further test units for bugs

--- bugs/tests.py
+++ bugs/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import bug_item
+from .forms import AddBugForm
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
 # Create your tests here.
 class bug_itemTests(TestCase):
@@ -19,6 +22,20 @@ class bug_itemTests(TestCase):
         test_author = bug_item(name='john')
         self.assertNotEqual(str(test_author), 'John')
         self.assertEqual(str(test_author), 'john')
-        
-        
 
+
+    def test_submit_bug_with_missing_fields(self):
+        form = AddBugForm(
+            {'name': '', 'description': 'lorem ipsum'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['name'], [u'This field is required.'])
+    
+    def test_create_bug(self):
+        self.user = User.objects.create(username='user')
+        self.entry = bug_item.objects.create(name='title', description='body',
+                                          author=self.user)
+    
+    def test_get_all_bugs(self):
+        page = self.client.get(reverse('show_bugs'))
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "bugs.html")
